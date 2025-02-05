34deaa59aaccecdc0b120bf9858cf0c905267237
Gabriel Gonzalez <ggonz296@stu.southtexascollege.edu>
2025-02-04 21:14:17 -0600

    Main foundation of my files

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup>
+//Global variables
+let selectedGender = "";
+let selectType ="";
+
+//Generate Contact function
+function addContact() {
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+
+    //validate inputs
+    if (name === "" || phone === "" || selectType === ""|| selectedGender === "") {
+        alert("Please enter name, phone number, contact type, and gender. Please click on the buttons.");
+        return;
+    }
+
+    //create a new list of contacts
+    let li = document.createElement("li");
+    li.textContent = `${"Name: " + name} - ${"Phone: " + phone} - ${"Contact Type: " + selectType} - 
+    (${"Gender: " + selectedGender})`;
+
+    //starts adding contacts to the list
+    document.getElementById("contactList").appendChild(li);
+
+    //clear inputs
+    selectedGender = "";
+    selectType ="";
+    document.getElementById("name").value = "";
+    document.getElementById("phone").value = "";
+}
+function setGender(gender) {
+    selectedGender = gender;
+    document.getElementById("maleButton");
+    document.getElementById("femaleButton");
+}
+function setType(type){
+    selectType = type;
+    document.getElementById("personalButton");
+    document.getElementById("workButton");
+}
+</script>
+
+<template>
+  <div class="content">
+        
+        <div>
+        <h1 class="main-title">Generate a List of Contacts</h1>
+        <h2>By Gabriel Gonzalez</h2>
+        <h2>For CITP-4316</h2>
+        <h2>Professor Perez</h2>
+        </div>
+
+        <div class="contactInput">
+            <label for="name">Name:</label>
+            <input type="text" id="name" placeholder="Pleae enter a name">
+            <label for="phone">Phone:</label>
+            <input type="tel" id="phone" placeholder="Please enter phonenumber">
+            <button onclick="addContact()">Add Contact</button>
+        </div>
+
+        <div class="Contact-Type">
+            <h3>Personal or Work Contact</h3>
+            <button onclick="setType('Personal')" id="personalButton">Personal</button>
+            <button onclick="setType('Work')" id="workButton">Work</button>
+        </div>
+
+        <div class = "Male-Female">
+            <h3>Please Select Contact Gender</h3>
+            <button onclick="setGender('Male')" id="maleButton">Male</button>
+            <button onclick="setGender('Female')" id="femaleButton">Female</button>
+        </div>
+
+        <div class="ContactList">
+            <div id="pending">
+            <h2>Contact List</h2>
+            <ul id="contactList">
+            </ul>
+            </div>
+        </div>
+
+    </div>
+</template>
